f9caaafe51c7f6ec744a108998ab069f94c5e904
Stanisław Stepka <s28016@pjwstk.edu.pl>
2024-04-20 02:09:31 +0200

    Dodanie komentarzy

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,8 +1,10 @@
 public class UserInterface {
     public static void main(String[] args) {
-        final String trainSetPath = args[0];
-        final String testSetPath = args[1];
+        final String trainSetPath = args[0]; // Ścieżka train-set.
+        final String testSetPath = args[1]; // Ścieżka test-set.
 
-
+        final BayesClassifier bayesClassifier = new BayesClassifier(); // Obiekt klasy BayesClassifier.
+        bayesClassifier.transformTrainSet(trainSetPath); // Procesowanie danych train-set.
+        bayesClassifier.testUsingTestSet(testSetPath); // Testowanie danych test-set
     }
 }
