fc6e5b5df5895cb499af0ab4a4195c4785335a00
Stanisław Stepka <s28016@pjwstk.edu.pl>
2024-04-20 02:09:48 +0200

    Implementacja metody readFromPath

--- src/IOUtility.java
+++ src/IOUtility.java
@@ -1,2 +1,18 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class IOUtility {
+    public static List<String> readFromPath(String stringPath) {
+        List<String> fileContents = null;
+
+        try {
+            fileContents = Files.readAllLines(Path.of(stringPath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return fileContents;
+    }
 }
